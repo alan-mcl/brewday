70de00b16905d080a3d2e5c31d1049cb418404bd
Alan McLachlan <alan.mcl@gmail.com>
2019-09-17 08:34:16 +0200

    adding sensitive data local storage to v2

---
+++ src/test/java/mclachlan/brewday/db/v2/sensitive/SensitiveStoreTest.java
@@ -0,0 +1,50 @@
+package mclachlan.brewday.db.v2.sensitive;
+
+import java.io.File;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class SensitiveStoreTest
+{
+	private String rootDir;
+
+	@Before
+	@After
+	public void setUp() throws Exception
+	{
+		new File(rootDir, "sensitive.v2").delete();
+		new File(rootDir, "dist.v2").delete();
+	}
+
+	private SensitiveStore getSensitiveStore() throws Exception
+	{
+		rootDir = "./test/resources/db/sensitive";
+		SensitiveStore ss = new SensitiveStore(rootDir);
+		ss.init("SensitiveStoreTest");
+
+		return ss;
+	}
+
+	@Test
+	public void init() throws Exception
+	{
+		getSensitiveStore();
+		return;
+	}
+
+	@Test
+	public void getAndSet() throws Exception
+	{
+		SensitiveStore ss = getSensitiveStore();
+
+		assertNull(ss.get("test"));
+
+		ss.set("xxx", "yyy");
+
+		assertEquals("yyy", ss.get("xxx"));
+	}
+}
