4708feb2f38bb16de8c65249af4c92db11110973
Alan McLachlan <alan.mcl@gmail.com>
2020-07-01 15:10:54 +0200

    gpl3 copyright headers

--- src/main/java/mclachlan/brewday/recipe/FermentableAddition.java
+++ src/main/java/mclachlan/brewday/recipe/FermentableAddition.java
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Brewday.  If not, see <https://www.gnu.org/licenses/>.
+ * along with Brewday.  If not, see https://www.gnu.org/licenses.
  */
 
 package mclachlan.brewday.recipe;
--- src/main/java/mclachlan/brewday/ui/swing/YeastTableModel.java
+++ src/main/java/mclachlan/brewday/ui/swing/YeastTableModel.java
@@ -6,13 +6,13 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Brewday is distributed in the yeaste that it will be useful,
+ * Brewday is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Brewday.  If not, see <https://www.gnu.org/licenses/>.
+ * along with Brewday.  If not, see https://www.gnu.org/licenses.
  */
 
 package mclachlan.brewday.ui.swing;
